37e87fdbed4509e2018afede6ea08069737cdd2d
eddielewis-thinhle <eddie.thinhle@outlook.com>
2019-11-04 14:24:42 +0700

    attempt to change meta data image x3

--- src/components/seo.js
+++ src/components/seo.js
@@ -70,7 +70,7 @@ function SEO({ description, lang, meta, title }) {
           },
           {
             name: `twitter:image`,
-            content: `./src/data/images/profile.jpg`,
+            content: `https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png`,
           },
         ].concat(meta)}
       />

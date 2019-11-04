ae42287f89ddddc3b392c45ae2b0f3167fb42190
eddielewis-thinhle <eddie.thinhle@outlook.com>
2019-11-04 12:49:05 +0700

    fix sitemap plugin declaration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     siteUrl: "https://eddie-thinhle.netlify.com/",
   },
   plugins: [
-    `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     {

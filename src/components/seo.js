82e1b59c6eada8dbffcc87190b20928fe9f0bf0b
eddielewis-thinhle <eddie.thinhle@outlook.com>
2019-11-04 14:05:08 +0700

    attempt to change meta data image

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,6 +68,10 @@ function SEO({ description, lang, meta, title }) {
             name: `twitter:description`,
             content: metaDescription,
           },
+          {
+            name: `twitter:image`,
+            content: ``,
+          },
         ].concat(meta)}
       />
       <Helmet>
